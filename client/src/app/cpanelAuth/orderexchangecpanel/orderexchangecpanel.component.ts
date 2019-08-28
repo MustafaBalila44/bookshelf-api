89b88e81cafebebff08cd6c4e14cd30842816d12
mnshorat <thawra206@gmail.com>
2019-08-28 18:00:58 +0200

    confilct

---
+++ client/src/app/cpanelAuth/orderexchangecpanel/orderexchangecpanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orderexchangecpanel',
+  templateUrl: './orderexchangecpanel.component.html',
+  styleUrls: ['./orderexchangecpanel.component.css']
+})
+export class OrderexchangecpanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/cpanelAuth/orderpurshasecpanel/orderpurshasecpanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orderpurshasecpanel',
+  templateUrl: './orderpurshasecpanel.component.html',
+  styleUrls: ['./orderpurshasecpanel.component.css']
+})
+export class OrderpurshasecpanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
